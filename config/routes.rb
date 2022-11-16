115dab722624db6832eb8443666bf37bc8450356
manuel esteves <mj.esteves92@gmail.com>
2022-11-16 16:55:46 +0000

    Yelp MVP | EX

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  post 'restaurants', to: 'restaurants#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
